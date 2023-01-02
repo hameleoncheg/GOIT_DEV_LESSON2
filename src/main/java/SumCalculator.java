293f4d8a160574861844aa14724c54a0c9aaac0b
hameleoncheg <hameleoncheg@gmail.com>
2023-01-02 19:18:58 +0200

    second commit

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,9 +1,5 @@
 public class SumCalculator {
 
-    public static void main(String[] args) {
-        System.out.println(sum(3));
-    }
-
     public  static int sum(int n) {
         int sum = 0;
         if (n <= 0) {
